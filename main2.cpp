c3876dc1d128aee2ae92616a02a91c1f6c90c5f3
Volkov17 <32384533+Volkov17@users.noreply.github.com>
2018-05-18 14:46:11 +0300

    Create main2.cpp

---
+++ main2.cpp
@@ -0,0 +1,14 @@
+   
+int main (){   
+   
+    std::cout<<" enter value "<<std::endl;
+    int a;
+    std::cin>>a;
+    std::cout" ennter grade" <<std::endl;
+    int b;
+    int res=1;
+    std::cin>>b;
+    for (int i =1 ;i<b;i++)
+        res*=a;
+    std::cout<< "answer"<<std::endl;
+}
